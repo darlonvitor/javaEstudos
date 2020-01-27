559dac7ce2029af97afb129d1cd377e38214aa3a
darlon vitor <darlonvitor@hotmail.com>
2020-01-27 19:44:43 -0300

    Criacao da classe SistermaInterno para realizacao de login

---
+++ src/heranca/SistemaInterno.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package heranca;
+
+import interfaces.Autenticavel;
+
+/**
+ *
+ * @author darlo
+ */
+public class SistemaInterno {
+    public void login(Autenticavel a){
+        int senha = 1234;
+        
+        boolean ok = a.autentica(senha);
+    }
+}
