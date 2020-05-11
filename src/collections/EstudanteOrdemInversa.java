e42314d58f42dce1a1493a107cca995870c14b04
darlon vitor <darlonvitor@hotmail.com>
2020-05-11 14:01:47 -0300

    Esta classe implementa Comparator para order a classe Estudante pela idade

---
+++ src/collections/EstudanteOrdemInversa.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package collections;
+
+/**
+ *
+ * @author darlo
+ */
+
+import java.util.Comparator;
+
+public class EstudanteOrdemInversa implements Comparator<Estudante> {
+
+    @Override
+    public int compare(Estudante o1, Estudante o2) {
+        return o2.getAge() - o1.getAge();//retorna a ordem decrescente da idade
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
